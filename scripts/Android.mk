647d833b3a8c83253478d1f6bebaf2fe68807acb
John Kirk <jkirk777@gmail.com>
2013-09-21 23:51:12 -0600

    Created GOGH specific compass and sensor scripts

--- scripts/Android.mk
+++ scripts/Android.mk
@@ -18,10 +18,26 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init.d
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := GOGH_compass
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := GOGH_compass
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init.d
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := 60sensors
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := 60sensors
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init.d
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := GOGH_sensors
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := GOGH_sensors
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init.d
 include $(BUILD_PREBUILT)
 
